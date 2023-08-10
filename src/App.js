8e7fecc27638b8dc93d2c1ad059144691c510a41
David Atanda <ololadedavid15@gmail.com>
2023-08-10 23:19:51 +0100

    routes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Admin from './pages/Admin';
 import Dashboard from './components/Dashboard';
+import Notification from './components/Notifications';
+import Users from './components/Users';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App main-content">
       <BrowserRouter>
         <Routes>
+          <Route path='/'  element={<Login />} />
           <Route path='/admin' element={<Admin />} >
             <Route path='/admin/' element={<Dashboard />} />
+            <Route path='/admin/notifications' element={<Notification />} />
+            <Route path='/admin/users' element={<Users />} />
           </Route>
         </Routes>
       </BrowserRouter>
