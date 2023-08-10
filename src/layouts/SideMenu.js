80aade3d3b1bd46ab4ffb7a300010b462a954d77
David Atanda <ololadedavid15@gmail.com>
2023-08-10 23:20:35 +0100

    icons added

--- src/layouts/SideMenu.js
+++ src/layouts/SideMenu.js
@@ -5,6 +5,7 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import { useLocation, useNavigate } from 'react-router';
+import { Dashboard, Logout, NotificationAdd, SupervisedUserCircle } from '@mui/icons-material';
 
 const SideMenu = ()=>{
     const navigate = useNavigate()
@@ -25,6 +26,7 @@ const SideMenu = ()=>{
                 <ListItem disablePadding>
                     <ListItemButton onClick={()=>navigate('/admin')} className={location.pathname == '/admin' ? 'active' : ''} >
                         <ListItemIcon>
+                            <Dashboard className={location.pathname == '/admin' ? 'text-dark' : 'text-white'} />
                         </ListItemIcon>
                         <ListItemText>
                             <p className={location.pathname == '/admin' ? 'my-0 fw-bold' : 'my-0'}>
@@ -38,6 +40,7 @@ const SideMenu = ()=>{
                 <ListItem disablePadding>
                     <ListItemButton onClick={()=>navigate('/admin/notifications')} className={location.pathname == '/admin/notifications' ? 'active' : ''} >
                         <ListItemIcon>
+                            <NotificationAdd className={location.pathname == '/admin/notifications' ? 'text-dark' : 'text-white'} />
                         </ListItemIcon>
                         <ListItemText>
                         <p className={location.pathname == '/admin/notifications' ? 'my-0 fw-bold' : 'my-0'}>
@@ -51,6 +54,7 @@ const SideMenu = ()=>{
                 <ListItem disablePadding>
                     <ListItemButton  onClick={()=>navigate('/admin/users')} className={location.pathname == '/admin/users' ? 'active' : ''} >
                         <ListItemIcon>
+                            <SupervisedUserCircle className={location.pathname == '/admin/users' ? 'text-dark' : 'text-white'} />
                         </ListItemIcon>
                         <ListItemText>
                         <p className={location.pathname == '/admin/users' ? 'my-0 fw-bold' : 'my-0'}>
@@ -62,8 +66,9 @@ const SideMenu = ()=>{
             </List>
             <List>
                 <ListItem disablePadding>
-                    <ListItemButton  onClick={()=>navigate('/login')} >
+                    <ListItemButton  onClick={()=>navigate('/')} >
                         <ListItemIcon>
+                            <Logout className='text-white' />
                         </ListItemIcon>
                         <ListItemText>
                             <p className='my-0'>
