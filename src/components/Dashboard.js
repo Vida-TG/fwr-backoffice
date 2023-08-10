fc1b1d837190f572bcc6da7e808b522979671b4e
David Atanda <ololadedavid15@gmail.com>
2023-08-10 11:45:18 +0100

    working on the dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,13 @@
+import { Paper } from "@mui/material";
 import React from "react";
 
 const Dashboard = ()=>{
     return (
-        <></>
+        <div className="p-5">
+            <Paper elevation={3} className="" >
+                Me
+            </Paper>
+        </div>
     )
 }
 export default Dashboard
