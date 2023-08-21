2e358b4fd7492c97516a0088f9048c4eae61ff00
David Atanda <ololadedavid15@gmail.com>
2023-08-21 23:55:58 +0100

    now fetching all notifications

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -1,16 +1,52 @@
 import { Paper } from "@mui/material";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 const Notification = ()=>{
+    const api = useSelector(state=>state.url)
+    const [error, setError] = useState('')
+    const [notificationTray, setNotificationTray] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(()=>{        
+        axios.get(`${api}admin/notifications`).then((res)=>{
+            setNotificationTray(res.data.notifications)
+            setIsLoading(false)
+            console.log(res.data)
+        }).catch((err)=>{
+            setIsLoading(false)
+            setError("Sorry, couldn't fetch from server")
+            console.log(err)
+        })
+    }, [])
     return (
         <div className="my-5">
             <p className="fs-5 pt-2 fw-less-bold text-white">
                 Notifications
             </p>
-            <Paper className="bg-dark text-white animate__animated animate__fadeIn animate__slow">
-                <p className="ps-3 pt-1">This is a notification</p>
-                <p className="text-end pe-3 pb-2">7:58pm</p>
-            </Paper>
+            {
+                isLoading
+                ?
+                <span className="spinner-border text-white"></span>
+                :
+                notificationTray.length !== 0 && !isLoading
+                ?
+                <Paper className="bg-dark text-white animate__animated animate__fadeIn animate__slow">
+                    <p className="ps-3 pt-1">This is a notification</p>
+                    <p className="text-end pe-3 pb-2">7:58pm</p>
+                </Paper>
+                :
+                notificationTray.length == 0 && error == '' && !isLoading
+                ?
+                <div className="alert alert-info">
+                    You haven't had any recent notification!
+                </div>
+                :
+                <div className="alert alert-danger">
+                    {error}
+                </div>
+            }
         </div>
     )
 }
