298fcbe1a8e407e5889e497c04f6d7eb62a08e56
David Atanda <ololadedavid15@gmail.com>
2023-08-22 18:44:30 +0100

    now showing notifications

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -32,14 +32,19 @@ const Notification = ()=>{
                 :
                 notificationTray.length !== 0 && !isLoading
                 ?
-                <Paper className="bg-dark text-white animate__animated animate__fadeIn animate__slow">
-                    <p className="ps-3 pt-1">This is a notification</p>
-                    <p className="text-end pe-3 pb-2">7:58pm</p>
-                </Paper>
+                notificationTray.map((each, i)=>(
+                    <Paper className="bg-dark text-white animate__animated animate__fadeIn animate__slow p-2">                    
+                        <div className="d-flex flex-md-row flex-column">
+                            <span className="fw-bold fs-6 text-fwr">New Registration Alert!</span>
+                            <span className="fw-light fs-6 text-fwr ps-3">{new Date(each.date).toUTCString()}</span>
+                        </div>  
+                        <p className="pt-1 px-3">{each.user_email} joined and registered as a Foreign WIves Reign member</p>                  
+                    </Paper>
+                ))  
                 :
                 notificationTray.length == 0 && error == '' && !isLoading
                 ?
-                <div className="alert alert-info">
+                <div className="alert alert-info fw-bold">
                     You haven't had any recent notification!
                 </div>
                 :
